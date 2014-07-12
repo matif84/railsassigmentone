04954a0a2eb702a6dc96892930939238e6b4b461
Muhammad Atif <muhammadatif@Bridge.local>
2014-07-12 17:31:34 -0400

    adding files

---
+++ controllers/pages_controller.rb
@@ -0,0 +1,14 @@
+class PagesController < ApplicationController
+
+	def home
+	puts "Hey I am Home!"
+	end
+
+	def cominghome
+	puts "Hey I am coming Home!"
+	end
+
+	def leavinghome
+	puts "Hey I am leaving Home!"
+	end
+end
